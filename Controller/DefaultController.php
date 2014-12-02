3935c77eb9691cd8d79fb97013d44c818a5b0353
Akeem <akeem.philbert@wepala.com>
2014-12-02 14:02:14 -0400

    Added default page for admin

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -1,1 +1,15 @@
 <?php
+namespace WeCMS\AdminBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="_we_admin")
+     * @Template(engine="php")
+     */
+    public function indexAction()
+    {
+        
+    }
+}
